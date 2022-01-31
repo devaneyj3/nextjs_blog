ff779894ae2cc35eb79738fc14f2378444ad10a3
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-31 17:14:25 -0500

    add layout component

---
+++ component/Layout/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MainNavigation from "./main-navigation";
+
+export default function Layout({ children }) {
+	return (
+		<>
+			<MainNavigation />
+			<main>{children}</main>
+		</>
+	);
+}
