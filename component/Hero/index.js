62c0a232b59757d79522c9e05e4e00759d711fb6
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-31 16:57:47 -0500

    add hero component

--- component/Hero/index.js
+++ component/Hero/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
 export default function Hero() {
-	return <div>Hero</div>;
+	return (
+		<section className={classes.hero}>
+			<div className={classes.image}>
+				<Image src="/images/me.png" alt="Jordan" width={300} height={300} />
+			</div>
+			<h1>Hi, I'm Jordan</h1>
+			<p>
+				I blog about Web Development - especially frontend libraries like React
+				and frameworks.
+			</p>
+		</section>
+	);
 }
