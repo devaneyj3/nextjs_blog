e76d5a7d09250dadbf2310b58cfe0ef6c07c24ec
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-31 21:14:33 -0500

    add post item component

--- component/PostItem/index.js
+++ component/PostItem/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Link from "next/link";
+
+import Image from "next/image";
 
 import classes from "./post-item.module.css";
 
-export default function PostItem() {
-	return <div></div>;
+export default function PostItem({ post }) {
+	const { title, image, excerpt, date, slug } = post;
+
+	const formattedDate = new Date(date).toLocaleDateString("en-US", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+
+	const imagePath = `/images/posts/${slug}/${image}`;
+	return (
+		<li className={classes.post}>
+			<Link>
+				<a>
+					<div className={classes.image}>
+						<Image src={imagePath} alt={title} width={300} height={200} />
+					</div>
+					<div className={classes.content}>
+						<h3>{title}</h3>
+						<time>{formattedDate}</time>
+						<p>{excerpt}</p>
+					</div>
+				</a>
+			</Link>
+		</li>
+	);
 }
